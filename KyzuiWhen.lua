52bc3e2701dd5eaec400cc34e162379493d3f3ba
Kyzderp <amurtiger224@gmail.com>
2020-09-27 07:30:08 -0700

    Initial commit with alkosh and colossus

---
+++ KyzuiWhen.lua
@@ -0,0 +1,117 @@
+-----------------------------------------------------------
+-- KyzuiWhen
+-- @author Kyzeragon
+-----------------------------------------------------------
+
+KyzuiWhen = {}
+KyzuiWhen.name = "KyzuiWhen"
+KyzuiWhen.version = "1.0.0"
+
+-- Defaults
+local defaultOptions = {
+    general = {
+        debug = true,
+    },
+    alkosh = {
+        enable = true,
+    },
+    colossus = {
+        enable = true,
+    },
+}
+
+local defaultValues = {
+}
+
+---------------------------------------------------------------------
+-- Initialize 
+function KyzuiWhen:Initialize()
+    -- Settings and saved variables
+    self.savedOptions = ZO_SavedVars:NewAccountWide("KyzuiWhenSavedVariables", 1, "Options", defaultOptions)
+    self.savedValues = ZO_SavedVars:NewAccountWide("KyzuiWhenSavedVariables", 1, "Values", defaultValues)
+
+    -- Initialize modules
+    ChatSpam:Initialize()
+
+    KyzuiWhen:dbg("KyzUI when???")
+end
+ 
+---------------------------------------------------------------------
+-- On Load
+function KyzuiWhen.OnAddOnLoaded(_, addonName)
+    if addonName == KyzuiWhen.name then
+        EVENT_MANAGER:UnregisterForEvent(KyzuiWhen.name, EVENT_ADD_ON_LOADED)
+        EVENT_MANAGER:RegisterForEvent(KyzuiWhen.name, EVENT_PLAYER_ACTIVATED, KyzuiWhen.OnPlayerActivated)
+        KyzuiWhen:Initialize()
+    end
+end
+ 
+EVENT_MANAGER:RegisterForEvent(KyzuiWhen.name, EVENT_ADD_ON_LOADED, KyzuiWhen.OnAddOnLoaded)
+
+---------------------------------------------------------------------
+-- Post Load (player loaded)
+function KyzuiWhen.OnPlayerActivated(_, initial)
+    EVENT_MANAGER:UnregisterForEvent(KyzuiWhen.name, EVENT_PLAYER_ACTIVATED)
+
+    -- Soft dependency on pChat because its chat restore will overwrite
+    for i = 1, #KyzuiWhen.messages do
+        d(KyzuiWhen.messages[i])
+    end
+    KyzuiWhen.messages = {}
+end
+
+-- Collect messages for displaying later when addon is not fully loaded
+KyzuiWhen.messages = {}
+function KyzuiWhen:dbg(msg)
+    if (not msg) then return end
+    if (not KyzuiWhen.savedOptions.general.debug) then return end
+    if (CHAT_SYSTEM.primaryContainer) then
+        d(msg)
+    else
+        KyzuiWhen.messages[#KyzuiWhen.messages + 1] = msg
+    end
+end
+
+---------------------------------------------------------------------
+-- Commands
+function KyzuiWhen.handleCommand(argString)
+    local args = {}
+    local length = 0
+    for word in argString:gmatch("%S+") do
+        table.insert(args, word)
+        length = length + 1
+    end
+
+    if (length == 0) then
+        CHAT_SYSTEM:AddMessage("Usage: /kw <show||debug||alkosh||colossus>")
+        return
+    end
+
+    -- ez print the stuff
+    if (args[1] == "show") then
+        CHAT_SYSTEM:AddMessage(string.format("All debug: %s", KyzuiWhen.savedOptions.general.debug and "|c00FF00on|r" or "|cFF0000off|r"))
+        CHAT_SYSTEM:AddMessage(string.format("Alkosh: %s", KyzuiWhen.savedOptions.alkosh.enable and "|c00FF00on|r" or "|cFF0000off|r"))
+        CHAT_SYSTEM:AddMessage(string.format("Colossus: %s", KyzuiWhen.savedOptions.colossus.enable and "|c00FF00on|r" or "|cFF0000off|r"))
+
+    -- Toggle debug
+    elseif (args[1] == "debug") then
+        KyzuiWhen.savedOptions.general.debug = not KyzuiWhen.savedOptions.general.debug
+        CHAT_SYSTEM:AddMessage("Debug (more like all printing) is now " .. tostring(KyzuiWhen.savedOptions.general.debug))
+
+    -- Toggle alkosh
+    elseif (args[1] == "alkosh") then
+        KyzuiWhen.savedOptions.alkosh.enable = not KyzuiWhen.savedOptions.alkosh.enable
+        CHAT_SYSTEM:AddMessage("Alkosh value chat spam is now " .. tostring(KyzuiWhen.savedOptions.alkosh.enable))
+
+    -- Toggle colossus
+    elseif (args[1] == "colossus") then
+        KyzuiWhen.savedOptions.colossus.enable = not KyzuiWhen.savedOptions.colossus.enable
+        CHAT_SYSTEM:AddMessage("Colossus invulnerability chat spam is now " .. tostring(KyzuiWhen.savedOptions.colossus.enable))
+
+    -- Unknown
+    else
+        CHAT_SYSTEM:AddMessage("Usage: /kw <debug>")
+    end
+end
+
+SLASH_COMMANDS["/kw"] = KyzuiWhen.handleCommand
---
+++ modules/ChatSpam.lua
@@ -0,0 +1,67 @@
+ChatSpam = {
+    units = {}, -- Attempt to cache unitIds to see what units get Alkosh/etc
+}
+
+function ChatSpam:Initialize()
+    KyzuiWhen:dbg("    Initializing ChatSpam module...")
+
+    -- Alkosh?
+    EVENT_MANAGER:RegisterForEvent(KyzuiWhen.name .. "ChatSpamAlkosh", EVENT_COMBAT_EVENT, ChatSpam.OnCombatAlkosh)
+    EVENT_MANAGER:AddFilterForEvent(KyzuiWhen.name .. "ChatSpamAlkosh", EVENT_COMBAT_EVENT, REGISTER_FILTER_COMBAT_RESULT, ACTION_RESULT_DAMAGE)
+    EVENT_MANAGER:AddFilterForEvent(KyzuiWhen.name .. "ChatSpamAlkosh", EVENT_COMBAT_EVENT, REGISTER_FILTER_ABILITY_ID, 75752)
+
+    -- Colossus?
+    EVENT_MANAGER:RegisterForEvent(KyzuiWhen.name .. "ChatSpamColossus", EVENT_EFFECT_CHANGED, ChatSpam.OnEffectColossus)
+    EVENT_MANAGER:AddFilterForEvent(KyzuiWhen.name .. "ChatSpamColossus", EVENT_EFFECT_CHANGED, REGISTER_FILTER_ABILITY_ID, 132831)
+end
+
+-- Print out Alkosh values in chat
+-- EVENT_COMBAT_EVENT (number eventCode, number ActionResult result, boolean isError, string abilityName, number abilityGraphic, number ActionSlotType abilityActionSlotType, string sourceName, number CombatUnitType sourceType, string targetName, number CombatUnitType targetType, number hitValue, number CombatMechanicType powerType, number DamageType damageType, boolean log, number sourceUnitId, number targetUnitId, number abilityId, number overflow)
+function ChatSpam.OnCombatAlkosh(_, _, _, abilityName, _, _, sourceName, _, targetName, _, hitValue, _, _, _, sourceUnitId, targetUnitId, abilityId, _)
+    if (not KyzuiWhen.savedOptions.alkosh.enable) then
+        return
+    end
+
+    -- Print Alkosh values depending on if it's from yourself or others
+    if (sourceName ~= nil and sourceName ~= "") then
+        KyzuiWhen:dbg(string.format("Self Alkosh |c00FF00%d|r on |c999999%s (%d)|r", hitValue, targetName, targetUnitId))
+        ChatSpam.units[targetUnitId] = targetName
+    elseif (ChatSpam.units[targetUnitId] ~= nil) then
+        KyzuiWhen:dbg(string.format("Other (%d) Alkosh |c00FFFF%d|r on |c999999%s (%d)|r", sourceUnitId, hitValue, ChatSpam.units[targetUnitId], targetUnitId))
+    else
+        KyzuiWhen:dbg(string.format("Other (%d) Alkosh |c00FFFF%d|r on |c999999Unknown (%d)|r", sourceUnitId, hitValue, targetUnitId))
+    end
+end
+
+-- Print out major vulnerability invulnerability in chat
+-- EVENT_EFFECT_CHANGED (number eventCode, MsgEffectResult changeType, number effectSlot, string effectName, string unitTag, number beginTime, number endTime, number stackCount, string iconName, string buffType, BuffEffectType effectType, AbilityType abilityType, StatusEffectType statusEffectType, string unitName, number unitId, number abilityId, CombatUnitType sourceType)
+function ChatSpam.OnEffectColossus(_, changeType, effectSlot, effectName, unitTag, beginTime, endTime, stackCount, _, buffType, effectType, abilityType, statusEffectType, unitName, unitId, abilityId, sourceType)
+    if (not KyzuiWhen.savedOptions.colossus.enable) then
+        return
+    end
+
+    -- KyzuiWhen:dbg(string.format("changeType %d, effectSlot %d, effectName %s, unitTag %s, beginTime %d, endTime %d, stackCount %d, buffType %s, effectType %d, abilityType %d, statusEffectType %d, unitName %s, unitId %s, abilityId %d, sourceType %d, abilityName %s", changeType, effectSlot, effectName, unitTag, beginTime, endTime, stackCount, buffType, effectType, abilityType, statusEffectType, unitName, unitId, abilityId, sourceType, GetAbilityName(abilityId)))
+    if (changeType == EFFECT_RESULT_GAINED) then
+        KyzuiWhen:dbg(string.format("|c999999%s (%d)|r |cFF0000gained|r Invulnerability", unitName, unitId))
+    elseif (changeType == EFFECT_RESULT_FADED) then
+        KyzuiWhen:dbg(string.format("|c999999%s (%d)|r |c00FF00lost|r Invulnerability", unitName, unitId))
+    end
+
+    ChatSpam.units[unitId] = unitName
+
+
+    -- [16:55:01] changeType 1, effectSlot 115, effectName Major Vulnerability Invulnerability, unitTag boss1, beginTime 2145, endTime 2165, stackCount 0, buffType , effectType 2, abilityType 0, statusEffectType 0, unitName Shademother^F, unitId 5601, abilityId 132831, sourceType 1, abilityName Major Vulnerability Immunity
+    -- [16:55:01] changeType 1, effectSlot 115, effectName Major Vulnerability Invulnerability, unitTag reticleover, beginTime 2145, endTime 2165, stackCount 0, buffType , effectType 2, abilityType 0, statusEffectType 0, unitName Shademother^F, unitId 5601, abilityId 132831, sourceType 1, abilityName Major Vulnerability Immunity
+    -- [16:55:03] changeType 1, effectSlot 80, effectName Major Vulnerability Invulnerability, unitTag , beginTime 2147, endTime 2167, stackCount 0, buffType , effectType 2, abilityType 0, statusEffectType 0, unitName Skeletal Dire Wolf^n, unitId 43086, abilityId 132831, sourceType 1, abilityName Major Vulnerability Immunity
+    -- [16:55:03] changeType 1, effectSlot 80, effectName Major Vulnerability Invulnerability, unitTag , beginTime 2147, endTime 2167, stackCount 0, buffType , effectType 2, abilityType 0, statusEffectType 0, unitName Skeletal Dire Wolf^n, unitId 18955, abilityId 132831, sourceType 1, abilityName Major Vulnerability Immunity
+
+    -- [16:55:04] changeType 2, effectSlot 80, effectName Major Vulnerability Invulnerability, unitTag , beginTime 0, endTime 0, stackCount 0, buffType , effectType 2, abilityType 0, statusEffectType 0, unitName Skeletal Dire Wolf^n, unitId 43086, abilityId 132831, sourceType 1, abilityName Major Vulnerability Immunity
+    -- [16:55:04] changeType 2, effectSlot 80, effectName Major Vulnerability Invulnerability, unitTag , beginTime 0, endTime 0, stackCount 0, buffType , effectType 2, abilityType 0, statusEffectType 0, unitName Skeletal Dire Wolf^n, unitId 18955, abilityId 132831, sourceType 1, abilityName Major Vulnerability Immunity
+    -- [16:55:21] changeType 2, effectSlot 115, effectName Major Vulnerability Invulnerability, unitTag boss1, beginTime 0, endTime 0, stackCount 0, buffType , effectType 2, abilityType 0, statusEffectType 0, unitName Shademother^F, unitId 5601, abilityId 132831, sourceType 1, abilityName Major Vulnerability Immunity
+    -- [16:55:21] changeType 2, effectSlot 115, effectName Major Vulnerability Invulnerability, unitTag reticleover, beginTime 0, endTime 0, stackCount 0, buffType , effectType 2, abilityType 0, statusEffectType 0, unitName Shademother^F, unitId 5601, abilityId 132831, sourceType 1, abilityName Major Vulnerability Immunity
+
+
+    -- group member Colossus, look when fading
+    -- [17:10:11] changeType 1, effectSlot 77, effectName Major Vulnerability Invulnerability, unitTag , beginTime 3055, endTime 3075, stackCount 0, buffType , effectType 2, abilityType 0, statusEffectType 0, unitName The Precursor, unitId 63074, abilityId 132831, sourceType 3, abilityName Major Vulnerability Immunity
+    -- [17:10:23] changeType 2, effectSlot 77, effectName Major Vulnerability Invulnerability, unitTag reticleover, beginTime 0, endTime 0, stackCount 0, buffType , effectType 2, abilityType 0, statusEffectType 0, unitName The Precursor, unitId 63074, abilityId 132831, sourceType 3, abilityName Major Vulnerability Immunity
+end
